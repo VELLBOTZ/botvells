f4f3d842b935cad32bbcaf6a731415ef5e25e099
Luffysanz <yosaardana7@gmail.com>
2021-12-16 08:04:28 +0700

    ravibotzz

---
+++ message/donasi.js
@@ -0,0 +1,15 @@
+exports.dodo = (prefix) => {
+return`
+╭────────❲ *DONASI* ❳
+│
+│  Bantu owner ngembangin bot ini
+│  dengan donasi seiklasnya 🙂
+│
+│  GOPAY: 087803515915
+│  DANA  : 087803515915
+│ 
+│ Makasih yang udah donasi
+│ dan bantu ngembangin bot ini
+│
+╰────────⏣`
+}
---
+++ message/sewabot.js
@@ -0,0 +1,54 @@
+
+exports.dada = (prefix, settings, pushname, ucapanWaktu) => {
+return `${ucapanWaktu} kak ${pushname}
+Berikut adalah list harga untuk sewa botz
+
+*╭─▸*「 𝗟𝗜𝗦𝗧 𝗦𝗘𝗪𝗔 」
+*┴*
+*│▸* 1 HARI = FREE
+*│▸* 1 MINGGU = 1K    
+*│▸* 1 BULAN = 5K      
+*│▸* PERMANEN = 10K              
+*┬*
+*╰────────────[*
+
+*╭─▸*「 𝗣𝗔𝗬𝗠𝗘𝗡𝗧 」
+*┴*
+*│▸* Gopay : ${settings.Gopay}
+*│▸* Dana : ${settings.Dana}
+*┬*
+*╰────────────[*
+
+𝗡𝗢𝗧𝗘 : 
+
+Bot on 24 jam tapi kadang 
+juga mati klo lgi ada error 
+atau lgi perbaikan bug.
+
+Kalo mau sewa bisa chat
+owner Bot, khusus untuk free 
+sewa 1 hari bisa pake fitur
+${prefix}freeoneday <Link Gc>
+tanpa harus chat owner
+`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
